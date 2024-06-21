5df5403fe7ffc3512c74d7d7e9e56a70c6b1db05
Hass <Dixon.Hass@dof.virginia.gov>
2024-06-21 16:08:25 -0400

    Added component and code to implement settings panel

---
+++ src/setting/setting.tsx
@@ -0,0 +1,8 @@
+
+import { React } from 'jimu-core'
+import { type AllWidgetSettingProps } from 'jimu-for-builder'
+const Setting = (props: AllWidgetSettingProps<any>) => {
+    return <div className="widget-setting-demo">This is your starter widget setting area!</div>
+    }
+    
+    export default Setting
