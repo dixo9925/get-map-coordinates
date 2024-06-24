6fc3fda9e5111fd5dd56538c535f173cfc57db27
dixo9925 <dhass1@vt.edu>
2024-06-24 10:16:11 -0400

    Further enabled map access and converted mouse cursor to point object

--- src/runtime/widget.tsx
+++ src/runtime/widget.tsx
@@ -3,7 +3,14 @@ import { JimuMapViewComponent, type JimuMapView } from 'jimu-arcgis'
 
 const Widget = (props: AllWidgetProps<any>) => {
   const { useState } = React
-  return <div className="widget_starter jimu-widget">This is your starter widget!</div>
+  const [latitude, setLatitude] = useState<string>('')
+  const [longitude, setLongitude] = useState<string>('')
+
+  return 
+  <div className="widget_starter jimu-widget"> {props.useMapWidgetIds && props.useMapWidgetIds.length === 1 && (
+    <JimuMapViewComponent useMapWidgetId={props.useMapWidgetIds?.[0]} onActiveViewChange={activeViewChangeHandler} />
+  )}
+  </div>
 }
 
 export default Widget
