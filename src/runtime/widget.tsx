40ed8c163291e65219c78d2ad6c57180fc37a5ba
Hass <dixon.hass@dof.virginia.gov>
2024-06-24 08:56:50 -0400

    Map access enabled

--- src/runtime/widget.tsx
+++ src/runtime/widget.tsx
@@ -1,6 +1,8 @@
 import { React, type AllWidgetProps } from 'jimu-core'
+import { JimuMapViewComponent, type JimuMapView } from 'jimu-arcgis'
 
 const Widget = (props: AllWidgetProps<any>) => {
+  const { useState } = React
   return <div className="widget_starter jimu-widget">This is your starter widget!</div>
 }
 
